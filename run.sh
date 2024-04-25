9d287da4651be2254fa61db723c543822bcf8717
puspawati121 <156059526+puspawati121@users.noreply.github.com>
2024-04-25 16:52:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"starknet_getBlockByNumber","params":["latest", false],"id":1}' https://rpc.starknet.lava.build/lava-referer-6526c639-29e5-4b0d-8854-c142949e5acb/)
 
 
 echo "$curl_output" >> Update.tsx
